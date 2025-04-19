95f37bf34d8ac6b406bc1ad17e1bd4f51ea5a27a
laurencelo1 <llo7@dons.usfca.edu>
2025-04-18 17:39:21 -0700

    feature(drag): able to drag cards between side and mainboard

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useOptimistic } from 'react';
+import { useState } from 'react';
 import { DragDropContext } from '@hello-pangea/dnd';
 import PoolBuilder from './PoolBuilder';
 
 const SIDE = [
   {
+    "name": "Sire of Seven Deaths",
+    "large_img": "https://cards.scryfall.io/large/front/8/d/8d8432a7-1c8a-4cfb-947c-ecf9791063eb.jpg?1730657490",
+    "cmc": 7,
+    "color": [],
+    "collection_number": "1",
+    "set_name": "FDN",
+  }, {
+    "name": "Camera Launcher",
+    "large_img": "https://cards.scryfall.io/large/front/9/6/968651db-92fb-46cd-acda-9e097668b7c9.jpg?1738356791",
+    "cmc": 3,
+    "color": [],
+    "collection_number": "232",
+    "set_name": "DFT",
+  }, {
     "name": "Sire of Seven Deaths",
     "large_img": "https://cards.scryfall.io/large/front/8/d/8d8432a7-1c8a-4cfb-947c-ecf9791063eb.jpg?1730657490",
     "cmc": 7,
@@ -40,16 +55,74 @@ const MAIN = [{
   "set_name": "DSK",
 }];
 
+
 export default function App() {
+  const [sideboardCards, setSideboardCards] = useState(SIDE);
+  const [mainboardCards, setMainboardCards] = useState(MAIN);
+
   const onDragEnd = (result) => {
-    console.log(`from ${result.source}`);
-    console.log(`to ${result.destination}`);
+    const { source, destination } = result;
+
+    if (!destination) {
+      console.log("destination is null!");
+      return;
+    }
+    if (source.droppableId === destination.droppableId && source.index === destination.index) {
+      console.log("drag drop in place!");
+      return;
+    }
+    
+    let movingCard;
+    // find moving card
+    if (source.droppableId === "sideboard") {
+      movingCard = sideboardCards[source.index];
+    } else if (source.droppableId === "mainboard") {
+      movingCard = mainboardCards[source.index];
+    }
+
+    // moving within
+    if (source.droppableId === destination.droppableId) {
+      if (source.droppableId === "sideboard") {
+        let newSideboard = Array.from(sideboardCards);
+        newSideboard.splice(source.index, 1);
+        newSideboard.splice(destination.index, 0, movingCard);
+        setSideboardCards(newSideboard);
+      } else if (source.droppableId === "mainboard") {
+        let newMainboard = Array.from(mainboardCards);
+        newMainboard.splice(source.index, 1);
+        newMainboard.splice(destination.index, 0, movingCard);
+        setMainboardCards(newMainboard);
+      }
+    } 
+    // moving to diff
+    else {
+      // side to main
+      if (source.droppableId === "sideboard") {
+        let newSideboard = Array.from(sideboardCards);
+        newSideboard.splice(source.index, 1);
+        setSideboardCards(newSideboard);
+
+        let newMainboard = Array.from(mainboardCards);
+        newMainboard.splice(destination.index, 0, movingCard);
+        setMainboardCards(newMainboard);
+      } 
+      // main to side
+      else {
+        let newMainboard = Array.from(mainboardCards);
+        newMainboard.splice(source.index, 1);
+        setMainboardCards(newMainboard);
+
+        let newSideboard = Array.from(sideboardCards);
+        newSideboard.splice(destination.index, 0, movingCard);
+        setSideboardCards(newSideboard);
+      }
+    }
   };
 
   return (
     <div className="app">
       <DragDropContext onDragEnd={onDragEnd}>
-        <PoolBuilder sideboard={SIDE} mainboard={MAIN} />
+        <PoolBuilder sideboard={sideboardCards} mainboard={mainboardCards} />
       </DragDropContext>
     </div>
   );
--- client/src/Card.js
+++ client/src/Card.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Draggable } from "@hello-pangea/dnd";
 import "./Card.css"
 
-function Card({id, index, name, color, cmc, imageUrl, cNum, codeSet}) {
-    console.log("Card ID:", id, "Index:", index);
+const Card = React.memo(function Card({ id, index, name, color, cmc, imageUrl, cNum }) {
+    // console.log("Card ID:", id, "Index:", index);
 
 
     function handleClick() {
@@ -17,19 +17,19 @@ function Card({id, index, name, color, cmc, imageUrl, cNum, codeSet}) {
                     ref={provided.innerRef}
                     {...provided.draggableProps}
                     {...provided.dragHandleProps}
-                    // className={`card ${snapshot.isDragging ? 'is-dragging' : ''}`}
-                    className="card"
+                    className={`card ${snapshot.isDragging ? 'is-dragging' : ''}`}
                 >
                     <img 
                         className="card"
                         src={imageUrl}
                         onClick={handleClick}
                         alt={name}
+                        loading="lazy"
                     />
                 </div>
             )}
         </Draggable>
-    )
-}
+    );
+});
 
 export default Card;
