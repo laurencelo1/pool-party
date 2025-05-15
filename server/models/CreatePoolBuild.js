a2a169ab23a19b9d621a0fce425a7526e505e151
laurencelo1 <llo7@dons.usfca.edu>
2025-05-14 22:48:19 -0700

    load mongoDB conn string from env to server file

---
+++ server/models/CreatePoolBuild.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+let schemaCourses = mongoose.Schema ({
+	side: {
+		type: Array,
+		required: true,
+		// index: {unique: true}
+	},
+	main: {
+		type: Array,
+		required: true,
+		// default: 'unknown'
+	},
+	preReqs: {
+		type: Array,
+		required: true,
+		default: ['unknown']
+	},
+	active: {
+		type: Boolean,
+		required: true,
+		default: true
+	},
+})
+
+module.exports = mongoose.model('CSCourse', schemaCourses);//Create and export model
--- server/server.js
+++ server/server.js
@@ -1,25 +1,35 @@
 import express from "express";
 import cors from "cors";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+import dotenv from "dotenv";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+dotenv.config({ path: join(__dirname, '..', '.env') });
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
+const HOST = 'localhost';
 
 app.use(cors());
 app.use(express.json());
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('../client/build'));
-    
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
-    });
-  }
   
-app.get('/api/hello', (req, res) => {
+app.get('/', (req, res) => {
+    console.log(process.env.MONGO_URI);
+
     res.json({ message: 'Hello from Pool Party API!' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static('../client/build'));
+    
+//     app.get('*', (req, res) => {
+//         res.sendFile(path.resolve(__dirname, '../client', 'build', 'index.html'));
+//     });
+//   }
