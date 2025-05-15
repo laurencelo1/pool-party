89d063326a431b8e75ebaf89bdc7caba74f6aa92
laurencelo1 <llo7@dons.usfca.edu>
2025-05-15 16:30:14 -0700

    CRUD pool with router

---
+++ server/routes/pool.route.js
@@ -0,0 +1,75 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import Pool from '../models/pool.model.js';
+
+const router = express.Router();
+
+// router.get('/', (req, res) => {
+//     res.json({ message: 'Hello from Pool Party API!' });
+// });
+
+router.post('/', async (req, res) => {
+    const pool = req.body;
+    console.log(req.body);
+    if (!pool.set || !pool.mainboard || !pool.sideboard || !pool.user) {
+        return res.status(400).json({ success:false, message: 'Please fill all fields'});
+    }
+
+    const newPool = new Pool(pool);
+
+    try {
+        await newPool.save();
+        res.status(201).json({ success:true, message: 'Pool submitted successfully', data: newPool});
+    } catch (error) {
+        console.error('submitting new pool' + error);
+        res.status(500).json({ success:false, message: 'Error submitting pool'});
+    }
+});
+
+router.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    console.log("id", id);
+
+    try {
+        const pool = await Pool.findByIdAndDelete(id);
+        if (!pool) {
+            return res.status(404).json({ success:false, message: 'Pool not found'});
+        }
+        res.status(200).json({ success:true, message: 'Pool deleted successfully', data: pool});
+    } catch (error) {
+        console.error('Error deleting pool' + error);
+        res.status(500).json({ success:false, message: 'Error deleting pool'});
+    }
+});
+
+router.get('/', async (req, res) => {
+    try {
+        const pools = await Pool.find();
+        res.status(200).json({ success:true, data: pools});
+    } catch (error) {
+        console.error('Error fetching pools' + error);
+        res.status(500).json({ success:false, message: 'Error fetching pools'});
+    }
+});
+
+router.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const pool = req.body;
+
+    if (!pool.set || !pool.mainboard || !pool.sideboard || !pool.user) {
+        return res.status(400).json({ success:false, message: 'Please fill all fields'});
+    }
+
+    try {
+        const updatedPool = await Pool.findByIdAndUpdate(id, pool, { new: true });
+        if (!updatedPool) {
+            return res.status(404).json({ success:false, message: 'Pool not found'});
+        }
+        res.status(200).json({ success:true, message: 'Pool updated successfully', data: updatedPool});
+    } catch (error) {
+        console.error('Error updating pool' + error);
+        res.status(500).json({ success:false, message: 'Error updating pool'});
+    }
+});
+
+export default router;
--- server/server.js
+++ server/server.js
@@ -7,6 +7,8 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import Pool from "./models/pool.model.js";
 
+import poolRoutes from "./routes/pool.route.js";
+
 // dotenv.config();
 
 
@@ -20,33 +22,11 @@ const HOST = 'localhost';
 
 app.use(cors());
 app.use(express.json());
-// console.log(process.env.MONGO_URI);
-  
-app.get('/', (req, res) => {
-    // console.log(process.env.MONGO_URI);
-
-    res.json({ message: 'Hello from Pool Party API!' });
-});
-
-app.post('/submit-pool', async (req, res) => {
-    const pool = req.body;
-    console.log(req.body);
-    if (!pool.set || !pool.mainboard || !pool.sideboard || !pool.user) {
-        return res.status(400).json({ success:false, message: 'Please fill all fields'});
-    }
-
-    const newPool = new Pool(pool);
-
-    try {
-        await newPool.save();
-        res.status(201).json({ success:true, message: 'Pool submitted successfully', data: newPool});
-    } catch (error) {
-        console.error('submitting new pool' + error);
-        res.status(500).json({ success:false, message: 'Error submitting pool'});
-    }
-});
+app.use("/pool", poolRoutes);
 
 app.listen(PORT, HOST, () => {
     connectDB();
     console.log(`Server running on port ${PORT}`);
 });
+// console.log(process.env.MONGO_URI);
+  
